f7ed892a908c328179723818276bf8e617800e14
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-02-22 21:18:44 +0300

    Add exercise_2

---
+++ Lab2/Lab2_exercise.pl
@@ -0,0 +1,304 @@
+race(manwe,1).
+race(ulmo,1).
+race(aule,1).
+race(orome,1).
+race(melkor,1).
+race(sauron,2).
+race(luthien,3).
+race(beren,4).
+race(thingol,3).
+race(melian,2).
+race(huan,7).
+race(carcharoth,7).
+race(finwe,3).
+race(feanor,3).
+race(fingolfin,3).
+race(maedhros,3).
+race(ungoliant,2).
+race(earendil,3).
+race(ancalagon,6).
+race(isildur,4).
+race(durin,5).
+race(glaurung,6).
+race(uldor,4).
+race(azaghal,5).
+race(turin,4).
+
+difference(manwe,1).
+difference(ulmo,2).
+difference(aule,3).
+difference(orome,4).
+difference(melkor,15).
+difference(sauron,15).
+difference(luthien,15).
+difference(beren,15).
+difference(thingol,15).
+difference(melian,15).
+difference(huan,15).
+difference(carcharoth,15).
+difference(finwe,15).
+difference(feanor,15).
+difference(fingolfin,15).
+difference(maedhros,15).
+difference(ungoliant,15).
+difference(earendil,15).
+difference(ancalagon,15).
+difference(isildur,15).
+difference(durin,15).
+difference(glaurung,15).
+difference(uldor,15).
+difference(azaghal,15).
+difference(turin,15).
+
+dark_lord(manwe,2).
+dark_lord(ulmo,2).
+dark_lord(aule,2).
+dark_lord(orome,2).
+dark_lord(melkor,1).
+dark_lord(sauron,1).
+dark_lord(luthien,2).
+dark_lord(beren,2).
+dark_lord(thingol,2).
+dark_lord(melian,2).
+dark_lord(huan,2).
+dark_lord(carcharoth,2).
+dark_lord(finwe,2).
+dark_lord(feanor,2).
+dark_lord(fingolfin,2).
+dark_lord(maedhros,2).
+dark_lord(ungoliant,2).
+dark_lord(earendil,2).
+dark_lord(ancalagon,2).
+dark_lord(isildur,2).
+dark_lord(durin,2).
+dark_lord(glaurung,2).
+dark_lord(uldor,2).
+dark_lord(azaghal,2).
+dark_lord(turin,2).
+
+theft_silmarils(manwe,2).
+theft_silmarils(ulmo,2).
+theft_silmarils(aule,2).
+theft_silmarils(orome,2).
+theft_silmarils(melkor,2).
+theft_silmarils(sauron,2).
+theft_silmarils(luthien,1).
+theft_silmarils(beren,1).
+theft_silmarils(thingol,1).
+theft_silmarils(melian,1).
+theft_silmarils(huan,1).
+theft_silmarils(carcharoth,2).
+theft_silmarils(finwe,2).
+theft_silmarils(feanor,2).
+theft_silmarils(fingolfin,2).
+theft_silmarils(maedhros,2).
+theft_silmarils(ungoliant,2).
+theft_silmarils(earendil,2).
+theft_silmarils(ancalagon,2).
+theft_silmarils(isildur,2).
+theft_silmarils(durin,2).
+theft_silmarils(glaurung,2).
+theft_silmarils(uldor,2).
+theft_silmarils(azaghal,2).
+theft_silmarils(turin,2).
+
+battle(manwe,2).
+battle(ulmo,2).
+battle(aule,2).
+battle(orome,2).
+battle(melkor,1).
+battle(sauron,1).
+battle(luthien,2).
+battle(beren,2).
+battle(thingol,2).
+battle(melian,2).
+battle(huan,2).
+battle(carcharoth,2).
+battle(finwe,2).
+battle(feanor,2).
+battle(fingolfin,2).
+battle(maedhros,1).
+battle(ungoliant,2).
+battle(earendil,2).
+battle(ancalagon,2).
+battle(isildur,2).
+battle(durin,2).
+battle(glaurung,1).
+battle(uldor,1).
+battle(azaghal,1).
+battle(turin,2).
+
+murder(manwe,2).
+murder(ulmo,2).
+murder(aule,2).
+murder(orome,2).
+murder(melkor,2).
+murder(sauron,2).
+murder(luthien,2).
+murder(beren,2).
+murder(thingol,2).
+murder(melian,2).
+murder(huan,2).
+murder(carcharoth,2).
+murder(finwe,1).
+murder(feanor,2).
+murder(fingolfin,1).
+murder(maedhros,2).
+murder(ungoliant,2).
+murder(earendil,2).
+murder(ancalagon,2).
+murder(isildur,2).
+murder(durin,2).
+murder(glaurung,2).
+murder(uldor,2).
+murder(azaghal,2).
+murder(turin,2).
+
+war(manwe,1).
+war(ulmo,1).
+war(aule,1).
+war(orome,1).
+war(melkor,1).
+war(sauron,1).
+war(luthien,2).
+war(beren,2).
+war(thingol,2).
+war(melian,2).
+war(huan,2).
+war(carcharoth,2).
+war(finwe,1).
+war(feanor,2).
+war(fingolfin,1).
+war(maedhros,2).
+war(ungoliant,2).
+war(earendil,1).
+war(ancalagon,1).
+war(isildur,2).
+war(durin,2).
+war(glaurung,2).
+war(uldor,2).
+war(azaghal,2).
+war(turin,1).
+
+mother(manwe,3).
+mother(ulmo,3).
+mother(aule,3).
+mother(orome,3).
+mother(melkor,3).
+mother(sauron,3).
+mother(luthien,3).
+mother(beren,3).
+mother(thingol,3).
+mother(melian,3).
+mother(huan,3).
+mother(carcharoth,3).
+mother(finwe,3).
+mother(feanor,1).
+mother(fingolfin,2).
+mother(maedhros,3).
+mother(ungoliant,3).
+mother(earendil,3).
+mother(ancalagon,3).
+mother(isildur,3).
+mother(durin,3).
+mother(glaurung,3).
+mother(uldor,3).
+mother(azaghal,3).
+mother(turin,3).
+
+royal(manwe,3).
+royal(ulmo,3).
+royal(aule,3).
+royal(orome,3).
+royal(melkor,3).
+royal(sauron,3).
+royal(luthien,1).
+royal(beren,3).
+royal(thingol,2).
+royal(melian,1).
+royal(huan,3).
+royal(carcharoth,3).
+royal(finwe,2).
+royal(feanor,2).
+royal(fingolfin,2).
+royal(maedhros,2).
+royal(ungoliant,3).
+royal(earendil,3).
+royal(ancalagon,3).
+royal(isildur,3).
+royal(durin,3).
+royal(glaurung,3).
+royal(uldor,3).
+royal(azaghal,3).
+royal(turin,3).
+
+question1(X1):- write("What is the race of this character?"),nl,
+		write("1. Ainur"),nl,
+		write("2. Maiar"),nl,
+		write("3. Quendi"),nl,
+		write("4. Atani"),nl,
+		write("5. Casari"),nl,
+		write("6. Dragons"),nl,
+		write("7. Other race"),nl,
+		read(X1).
+		
+question2(X2):- write("How is the character different?"),nl,
+		write("1. Is the Master of the sky, wind, air and clouds"),nl,
+		write("2. Is the Master of all Waters"),nl,
+		write("3. Is the Master of the terra firma, mountains and metal"),nl,
+		write("4. Is the Master of the woods, the patron saint of hunting"),nl,
+		write("5. Is the Judge of the Dead and the Master of Doom"),nl,
+		write("6. Is the character the Master of dreams"),nl,
+		write("7. Has the great physical power"),nl,
+		write("8. Is the lady of the stars"),nl,
+		write("9. Is the patroness of plants, animals and birds"),nl,
+		write("10. Is the lady of pity, grief and hope"),nl,
+		write("11. Has the power to heal of wounds and mental fatigue"),nl,
+		write("12. Created tapestries of Arda’s fate"),nl,
+		write("13. Has eternal youth, connection with nature"),nl,
+		write("14. Is very fast and agile and is the wife of Tulkas"),nl,
+		write("15. Other feature"),nl,
+		read(X2).
+
+question3(X3):- write("Was the character a Dark Lord?"),nl,
+		write("1. Yes"),nl,
+		write("2. No"),nl,
+		read(X3).
+
+question4(X4):- write("Was the character involved in the theft of one of the Silmarils from the crown of Morgoth?"),nl,
+		write("1. Yes"),nl,
+		write("2. No"),nl,
+		read(X4).
+		
+question5(X5):- write("Did the character participate in the Battle of Unnumbered Tears?"),nl,
+		write("1. Yes"),nl,
+		write("2. No"),nl,
+		read(X5).
+		
+question6(X6):- write("Did Morgoth kill the character?"),nl,
+		write("1. Yes"),nl,
+		write("2. No"),nl,
+		read(X6).
+		
+question7(X7):- write("Did the character participate in the War of Wrath?"),nl,
+		write("1. Yes"),nl,
+		write("2. No"),nl,
+		read(X7).
+		
+question8(X8):- write("Who is the character’s mother?"),nl,
+		write("1. Miriel"),nl,
+		write("2. Indis"),nl,
+		write("3. Other variant"),nl,
+		read(X8).
+
+question9(X9):- write("Is the character a member of the Elven royal family?"),nl,
+		write("1. Yes, she is a princess/queen"),nl,
+		write("2. Yes, he is prince/king"),nl,
+		write("3. No"),nl,
+		read(X9).
+		
+pr:- question1(X1),question2(X2),question3(X3),question4(X4),question5(X5),
+	question6(X6),question7(X7),question8(X8),question9(X9),
+	race(X,X1),difference(X,X2),dark_lord(X,X3),theft_silmarils(X,X4),
+	battle(X,X5),murder(X,X6),war(X,X7),mother(X,X8),royal(X,X9),
+	write(X).
