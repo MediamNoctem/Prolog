880ba984c1846a310f1cdcbff5f38e297d6281fb
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-02-23 17:57:43 +0300

    Add exercise 7

--- Lab3/Lab3_exercise.pl
+++ Lab3/Lab3_exercise.pl
@@ -19,3 +19,6 @@ fib(N,X):-N1 is N-1,fib(N1,X1),N2 is N-2,fib(N2,X2),X is X1+X2.
 fib(N,_,X,N,X):-!.
 fib(I,F1,F2,N,X):-I1 is I+1, F3 is F1+F2,fib(I1,F2,F3,N,X).
 fib_down(N,X):-N>0,fib(1,0,1,N,X).
+
+sum_digits_up(0,0):-!.
+sum_digits_up(N,S):-N1 is N div 10, sum_digits_up(N1,S1), S is S1 + N mod 10.
