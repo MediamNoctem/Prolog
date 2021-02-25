438cc8c472055ce7af304350f1cfdc6c5dd95e6c
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-02-25 17:39:32 +0300

    Add exercise 4.2

--- Lab4/Lab4_exercise.pl
+++ Lab4/Lab4_exercise.pl
@@ -16,3 +16,9 @@ sum_list_up([Head|Tail],Summ):- sum_list_up(Tail,S1), Summ is Head + S1.
 list_el_numb(List,Elem,Number):- list_el_numb(List,Elem,0,Number).
 list_el_numb([Head|_],Head,Number,Number):-!.
 list_el_numb([_|Tail],Elem,I,Number):- I1 is I+1, list_el_numb(Tail,Elem,I1,Number).
+
+pr4_4:- write("Enter the number: "), read(N), write("Enter the list:"), nl,
+	read_list(N,List), write("Enter an item: "), read(Elem), list_el_numb(List,Elem,Number),
+	write("Number = "), write(Number),!.
+pr4_4:- write("There is no such item in the list.").
+
