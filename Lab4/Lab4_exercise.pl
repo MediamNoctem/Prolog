da2c4009f3b8d90a44756c54b22c7cc037ebfd47
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-02-25 16:21:16 +0300

    Add exercise 3

--- Lab4/Lab4_exercise.pl
+++ Lab4/Lab4_exercise.pl
@@ -9,3 +9,7 @@ sum_list_down([],Summ,Summ):-!.
 sum_list_down([Head|Tail],S,Summ):- S1 is S + Head, sum_list_down(Tail,S1,Summ).
 pr4_2:- write("Enter the number:"), nl, read(N), write("Enter the list:"), nl, 
 	read_list(N,List), sum_list_down(List,Summ), write("Summ = "), write(Summ).
+	
+sum_list_up([],0):-!.
+sum_list_up([Head|Tail],Summ):- sum_list_up(Tail,S1), Summ is Head + S1.
+
