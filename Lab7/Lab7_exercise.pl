654ef16ff647be5ab753b6d0486a7d30de967c64
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-03-12 17:14:20 +0300

    Add exercise 2

--- Lab7/Lab7_exercise.pl
+++ Lab7/Lab7_exercise.pl
@@ -8,3 +8,12 @@ r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
 % 1
 p1:- read_str(List,Length), write_str(List), write(", "), write_str(List), 
 	write(", "), write_str(List), nl, write("Length = "), write(Length).
+	
+% 2
+p2:- write("Enter a string without spaces at the beginning and end."), nl,
+	read_str(List,_), count_words(List,0,K), 
+	(K\=0 -> Num is K+1, write("Number = "), write(Num); write("Empty!")),!.
+	
+count_words([],K,K):-!.
+count_words([H|T],I,K):- H = 32, I1 is I+1, count_words(T,I1,K).
+count_words([_|T],I,K):- count_words(T,I,K).
