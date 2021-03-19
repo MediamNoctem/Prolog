b55207ffcbdda9c20fdbeb2abc15ee95475a4f7d
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-03-19 21:30:39 +0300

    Update exercise 2 and add exercise 3

--- Lab7/Lab7_exercise.pl
+++ Lab7/Lab7_exercise.pl
@@ -1,4 +1,4 @@
-write_str([]):-!.
+﻿write_str([]):-!.
 write_str([H|Tail]):-put(H),write_str(Tail).
 
 read_str(A,N):-get0(X),r_str(X,A,[],N,0).
@@ -10,10 +10,65 @@ p1:- read_str(List,Length), write_str(List), write(", "), write_str(List),
 	write(", "), write_str(List), nl, write("Length = "), write(Length).
 	
 % 2
-p2:- write("Enter a string without spaces at the beginning and end."), nl,
-	read_str(List,_), count_words(List,0,K), 
-	(K\=0 -> Num is K+1, write("Number = "), write(Num); write("Empty!")),!.
+p2:- read_str(List,_), append([32], List, List1), count_words(List1,0,K),
+	(K\=0 -> write("Number of words = "), write(K); write("Empty!")),!.
 	
 count_words([],K,K):-!.
-count_words([H|T],I,K):- H = 32, I1 is I+1, count_words(T,I1,K).
-count_words([_|T],I,K):- count_words(T,I,K).
+count_words([32,H2|T],I,K):- H2\=32, H2\=10, I1 is I+1, count_words(T,I1,K).
+count_words([_|T],I,K):- count_words(T,I,K),!.
+
+% 3
+in_list([El|_],El):-!.
+in_list([_|T],El):-in_list(T,El).
+
+%p3:- read_str(List,_),
+
+% Извлекаем слово из списка. В начале не должно быть пробела.
+% get_word(+List, Word).
+
+get_word([32|_],[]):- !.
+get_word([H|T1],[H|T2]):- H\=32, get_word(T1,T2),!.
+
+% Считаем, сколько раз слово встречается в списке. В конце списка должен быть пробел.
+% num_word(+List,+Word,+Current_number[=0],Number).
+
+num_word([],_,I,I):-!.
+num_word([32|T],Word,I,C):- delete_space(T,List), 
+	num_word(List,Word,I,C), !.
+num_word(List,Word,I,C):- get_word(List,W), delete_fword(List,L),
+	(W = Word -> I1 is I+1; I1 is I), num_word(L,Word,I1,C).
+	
+append_list([],List2,List2).
+append_list([H|T1],List2,[H|T2]):- append_list(T1,List2,T2).
+
+delete_space([32|T],List):- delete_space(T,List),!.
+delete_space(List,List).
+
+% Удаление первого слова. В начале не должно быть пробела. 
+% В конце списка должен быть пробел.
+delete_fword([H|T1],List):- H\=32, delete_fword(T1,List),!.
+delete_fword([32|T],T):-!.
+
+% Ищем самое часто встречающееся слово в списке.
+% freq_word(+List,Freq_word).
+
+freq_word(List,Freq_word):- delete_space(List,L), append_list(L,[32],Source_list),
+	freq_word(Source_list,Source_list,[],0,Freq_word).
+freq_word(_,[],Word,_,Word):-!.
+freq_word(Source_list,Changing_list,Current_word,Num,Freq_word):-
+	get_word(Changing_list,Cur), num_word(Source_list,Cur,0,I), 
+	delete_fword(Changing_list, List), delete_space(List,Changed_list),
+	(I > Num -> freq_word(Source_list,Changed_list,Cur,I,Freq_word);
+	freq_word(Source_list,Changed_list,Current_word,Num,Freq_word)).
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
