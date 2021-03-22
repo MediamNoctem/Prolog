516053d21e97966104c577ca4252928023829475
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-03-22 11:12:59 +0300

    Add exercise 1_4

--- Lab8/Lab8_exercise.pl
+++ Lab8/Lab8_exercise.pl
@@ -1,4 +1,4 @@
-write_str([]):-!.
+﻿write_str([]):-!.
 write_str([H|Tail]):-put(H),write_str(Tail).
 
 read_str(A,N,Flag):-get0(X),r_str(X,A,[],N,0,Flag).
@@ -69,4 +69,54 @@ read_list_str_A(List,M,0):-
 	read_str_symbol(A,Count,65,Flag), (Count > M -> write_str(A),nl; true),
 	read_list_str_A(List,M,Flag).
 	
+% 1_4
+p1_4:- see('c:/Users/Anastasia/Desktop/p1_in.txt'), read_list_str1(List), seen,
+	freq_word(List,Freq_word), (Freq_word\=[] -> write("Word = "), write_str(Freq_word); false).
 	
+read_list_str1(List):- read_str(A,_,Flag), append(A,[32],L),
+	read_list_str1(L,List,Flag).
+read_list_str1(List,List,1):-!.
+read_list_str1(Cur_list,List,0):-
+	read_str(A,_,Flag), append(Cur_list,A,C_l), append(C_l,[32],L), 
+	read_list_str1(L,List,Flag).
+
+in_list([El|_],El):-!.
+in_list([_|T],El):-in_list(T,El).
+
+% Извлекаем слово из списка. В начале не должно быть пробела.
+% get_word(+List, Word).
+
+get_word([32|_],[]):- !.
+get_word([H|T1],[H|T2]):- H\=32, get_word(T1,T2),!.
+
+% Считаем, сколько раз слово встречается в списке. В конце списка должен быть пробел.
+% num_word(+List,+Word,+Current_number[=0],Number).
+
+num_word([],_,I,I):-!.
+num_word([32|T],Word,I,C):- delete_space(T,List),
+	num_word(List,Word,I,C), !.
+num_word(List,Word,I,C):- get_word(List,W), delete_fword(List,L),
+	(W = Word -> I1 is I+1; I1 is I), num_word(L,Word,I1,C).
+
+append_list([],List2,List2).
+append_list([H|T1],List2,[H|T2]):- append_list(T1,List2,T2).
+
+delete_space([32|T],List):- delete_space(T,List),!.
+delete_space(List,List).
+
+% Удаление первого слова. В начале не должно быть пробела.
+% В конце списка должен быть пробел.
+delete_fword([H|T1],List):- H\=32, delete_fword(T1,List),!.
+delete_fword([32|T],T):-!.
+
+% Ищем самое часто встречающееся слово в списке.
+% freq_word(+List,Freq_word).
+
+freq_word(List,Freq_word):- delete_space(List,L), append_list(L,[32],Source_list),
+	freq_word(Source_list,Source_list,[],0,Freq_word).
+freq_word(_,[],Word,_,Word):-!.
+freq_word(Source_list,Changing_list,Current_word,Num,Freq_word):-
+	get_word(Changing_list,Cur), num_word(Source_list,Cur,0,I),
+	delete_fword(Changing_list, List), delete_space(List,Changed_list),
+	(I > Num -> freq_word(Source_list,Changed_list,Cur,I,Freq_word);
+	freq_word(Source_list,Changed_list,Current_word,Num,Freq_word)).
