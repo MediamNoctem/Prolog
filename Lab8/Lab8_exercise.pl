0b1bc384a20cfb4c478e936e019f5f435750aef0
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-03-22 09:08:15 +0300

    Add exercise 1_2

--- Lab8/Lab8_exercise.pl
+++ Lab8/Lab8_exercise.pl
@@ -15,8 +15,8 @@ read_list_str(List,List,1):-!.
 read_list_str(Cur_list,List,0):-
 	read_str(A,_,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
 
-% 1
-p1:- see('c:/Users/Anastasia/Desktop/p1_in.txt'), read_list_str(_, LengthList), 
+% 1_1
+p1_1:- see('c:/Users/Anastasia/Desktop/p1_in.txt'), read_list_str(_, LengthList), 
 	seen, max(LengthList, Max), write(Max).
 
 read_list_str(List, LengthList):-read_str(A,N,Flag),read_list_str([A],List,[N],LengthList,Flag).
@@ -28,3 +28,23 @@ max(List, MaxEl):- max(List, 0, MaxEl).
 max([],CurMax, CurMax):- !.
 max([H|T], CurMax, X):- H > CurMax, NewMax is H, max(T,NewMax,X), !.
 max([_|T], CurMax, X):- max(T, CurMax, X).
+
+% 1_2
+p1_2:- see('c:/Users/Anastasia/Desktop/p1_in.txt'), read_list_str_space(_, C), 
+	seen, write("C = "), write(C).
+
+read_str_space(A,Count,Flag):- get0(X), r_str_space(X,A,[],Count,0,Flag).
+r_str_space(-1,A,A,Count,Count,1):-!.
+r_str_space(10,A,A,Count,Count,0):-!.
+r_str_space(X,A,B,Count,C,Flag):- append(B,[X],B1), get0(X1),
+	(X = 32 -> C1 is C+1; C1 is C), r_str_space(X1,A,B1,Count,C1,Flag).
+
+read_list_str_space(List, C):- read_str_space(A,Count,Flag), 
+	(Count = 0 -> C1 = 1; C1 = 0), read_list_str_space([A],List,C1,C,Flag).
+	
+read_list_str_space(List,List,C,C,1):-!.
+
+read_list_str_space(Cur_list,List,CurC,C,0):-
+	read_str_space(A,Count,Flag), append(Cur_list,[A],C_l),
+	(Count = 0 -> C1 is CurC+1; C1 is CurC),
+	read_list_str_space(C_l,List,C1,C,Flag).
