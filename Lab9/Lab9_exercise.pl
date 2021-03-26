bfb0fbea651243605162c2ae5612d8f7ee9a22f6
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-03-26 13:27:14 +0300

    Add exercise 3

--- Lab9/Lab9_exercise.pl
+++ Lab9/Lab9_exercise.pl
@@ -5,6 +5,9 @@ read_str(A,N):- get0(X), r_str(X,A,[],N,0).
 r_str(10,A,A,N,N):-!.
 r_str(X,A,B,N,K):- K1 is K+1, append(B,[X],B1), get0(X1), r_str(X1,A,B1,N,K1).
 
+write_list([]):-!.
+write_list([Head|Tail]):- write(Head), write_list(Tail).
+
 write_str([]):-!.
 write_str([H|Tail]):- put(H), write_str(Tail).
 
@@ -62,7 +65,7 @@ pr2:- Positions = [0,1,2,3,4], Itog_slovo = [_,_,_,_,_],
 	in_list([b,c,d,e,f],El1), put_free(Itog_slovo,El1),
 	in_list([b,c,d,e,f],El2), put_free(Itog_slovo,El2),
 	in_list([b,c,d,e,f],El3), put_free(Itog_slovo,El3),
-	write(Itog_slovo), nl, fail.
+	write_list(Itog_slovo), nl, fail.
 
 % put_letter(Word,Letter_positions,Letter).
 % Ставим букву Letter на все позиции Letter_positions в списке Word.
@@ -72,3 +75,12 @@ put_letter([_|T],Letter_positions,I,L):- I1 is I+1, put_letter(T,Letter_position
 
 put_free([H|_],L):- var(H), H = L,!.
 put_free([_|T],L):- put_free(T,L),!.
+
+% 3
+p3:- tell('c:/Users/Anastasia/Desktop/p1_out.txt'), pr3, told.
+pr3:- Positions = [0,1,2,3,4], Itog_slovo = [_,_,_,_,_], 
+	sochet(A_positions,2,Positions), put_letter(Itog_slovo,A_positions,0,a),
+	in_list_exlude([b,c,d,e,f],El1,A1), put_free(Itog_slovo,El1),
+	in_list_exlude(A1,El2,A2), put_free(Itog_slovo,El2),
+	in_list(A2,El3), put_free(Itog_slovo,El3),
+	write_list(Itog_slovo), nl, fail.
