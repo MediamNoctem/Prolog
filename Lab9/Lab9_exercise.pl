5992ac96a0bb05ba08541fca073542fc3408817a
MediamNoctem <76221937+MediamNoctem@users.noreply.github.com>
2021-03-26 09:43:45 +0300

    Add exercise 1_3

--- Lab9/Lab9_exercise.pl
+++ Lab9/Lab9_exercise.pl
@@ -29,3 +29,10 @@ in_list_exlude([H|T],El,[H|Tail]):- in_list_exlude(T,El,Tail).
 
 b_a_p([],Perm1):- write_str(Perm1), nl, !, fail.
 b_a_p(A,Perm):- in_list_exlude(A,El,A1), b_a_p(A1,[El|Perm]).
+
+% 1_3
+p1_3:- tell('c:/Users/Anastasia/Desktop/p1_out.txt'), build_all_razm, told.
+build_all_razm:- read_str(A,_,_), read(K), b_a_r(A,K,[]).
+		
+b_a_r(_,0,Perm1):- write_str(Perm1), nl, !, fail.
+b_a_r(A,N,Perm):- in_list_exlude(A,El,A1), N1 is N-1, b_a_r(A1,N1,[El|Perm]).
